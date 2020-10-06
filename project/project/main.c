1cfc6a0d3997a685fc6c0507d627766173979ae1
yujinlee325 <yu_jin325@naver.com>
2020-10-06 16:10:27 +0900

    1006_3

--- project/project/main.c
+++ project/project/main.c
@@ -10,15 +10,19 @@
 
 int main(int argc, const char * argv[]) {
     
-    int i;
+    int num=0;
+    char c;
     
-        printf("정수 하나를 입력하시오.: ");
-        scanf("%i", &i);
+        printf("Input a string: ");
         
-        if(i < 0)
-            i = i*(-1);
-    
-            printf("절댓값은 %i 입니다.\n", i);
+        while((c=getchar()) != '\n')
+        {
+            if( c >= '0' && c <= '9')
+            {
+                num++;
+            }
+        }
+    printf("the number of digits is %i\n", num);
        
     return 0;
 }
